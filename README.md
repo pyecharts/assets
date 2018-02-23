f2d8178de78a4c54de0e398f3fd95358438443a5
chfw <wangc_2011@hotmail.com>
2018-02-23 21:19:32 +0000

    :tada: first commit to support pyecharts 0.3.2+ release

---
+++ README.md
@@ -0,0 +1,8 @@
+# assets
+
+For django and Flask developers, this project is the turn key solution for hosting pyecharts javascript assets. It
+can be placed as it is into your server and its gh-pages acts as the default content delivery host when you evaluate pyecharts for web purposes.
+
+## license
+
+It includes echarts from Baidu Inc.
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp')
+var rename = require('gulp-rename');
+
+const FILES =[
+  '!./node_modules/jupyter-echarts/echarts/main.js',
+  '!./node_modules/echarts-countries-js/echarts-countries-js/index.js',
+  '!./node_modules/echarts-china-provinces-js/echarts-china-provinces-js/index.js',
+  '!./node_modules/echarts-china-cities-js/echarts-china-cities-js/index.js',
+  './node_modules/jupyter-echarts/echarts/*.js',
+  './node_modules/echarts-countries-js/echarts-countries-js/*.js',
+  './node_modules/echarts-china-provinces-js/echarts-china-provinces-js/*.js',
+  './node_modules/echarts-china-cities-js/echarts-china-cities-js/*.js'
+]
+       
+gulp.task('default', function(){
+  gulp.src(FILES)
+    .pipe(rename({dirname:''}))
+    .pipe(gulp.dest('js'));
+});
