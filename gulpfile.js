e0d21f7ce5733dc1ff62e960b76cf45b0bc95be8
chfw <wangc_2011@hotmail.com>
2018-05-12 22:23:18 +0100

    :sparkles: include echarts-china-counties-js and echarts-themes-js

--- gulpfile.js
+++ gulpfile.js
@@ -7,9 +7,11 @@ const FILES =[
   '!./node_modules/echarts-china-provinces-js/echarts-china-provinces-js/index.js',
   '!./node_modules/echarts-china-cities-js/echarts-china-cities-js/index.js',
   './node_modules/jupyter-echarts/echarts/*.js',
+  './node_modules/echarts-themes-js/echarts-themes-js/*.js',
   './node_modules/echarts-countries-js/echarts-countries-js/*.js',
   './node_modules/echarts-china-provinces-js/echarts-china-provinces-js/*.js',
   './node_modules/echarts-china-cities-js/echarts-china-cities-js/*.js',
+  './node_modules/echarts-china-counties-js/echarts-china-counties-js/*.js',
   './node_modules/echarts-china-misc-js/echarts-china-misc-js/*.js',
   './node_modules/echarts-united-kingdom-js/echarts-united-kingdom-js/*.js'
 ]
